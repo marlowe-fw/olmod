d901b94031c0685a7bd7fb7bff9d588bda5c348b
Jochai von Baumgarten <Jochai.von.Baumgarten@laerdal.com>
2021-05-27 22:07:53 +0200

    PoC: Auto start lan match

--- GameMod/MPBotFramework/PatchBotApplication.cs
+++ GameMod/MPBotFramework/PatchBotApplication.cs
@@ -73,26 +73,94 @@ namespace GameMod.MPBotFramework
 
                             if (keyCode == KeyCode.KeypadEnter)
                             {
-                                UIManager.DestroyAll(false);
-                                UIManager.DestroyAll(false);
-                                NetworkMatch.SetNetworkGameClientMode(NetworkMatch.NetworkGameClientMode.LocalLAN);
-                                MPInternet.MenuPassword = "127.0.0.1";
-                                MenuManager.ChangeMenuState(MenuState.MP_LOCAL_MATCH, false);
-                                UIManager.m_menu_selection = 1;
-                                MenuManager.m_menu_state = MenuState.MP_LOCAL_MATCH;
-                                MenuManager.PlaySelectSound(1f);
+                            //UIManager.DestroyAll(false);
 
-                                // ---
 
-                                //var x = MenuState.MP_LOCAL_MATCH;
-                                //UIManager.m_menu_selection = 7;
-                                //MenuManager.m_mp_lan_match = true;
-                                //(MenuManager.m_menu_micro_state == 0 && UIManager.m_menu_selection == 2) // create open match
+                            //InternetMatch.Enabled = true;
 
-                            }
+                            // -- Switch to match setup (LAN)
 
-                            //Debug.Log("KeyCode down: " + keyCode);
-                            break;
+
+                            /*
+                            UIManager.DestroyAll(false);
+                            MenuManager.ChangeMenuState(MenuState.MP_LOCAL_MATCH, false);
+                            MenuManager.m_mp_lan_match = true;
+                            MenuManager.m_mp_private_match = true;
+                            MenuManager.m_menu_micro_state = 4;
+                            NetworkMatch.SetNetworkGameClientMode(NetworkMatch.NetworkGameClientMode.LocalLAN);
+                            MenuManager.PlaySelectSound(1f);
+                            UIManager.CreateUIElement(UIManager.SCREEN_CENTER, 7000, UIElementType.MP_LOBBY MP_MATCH_SETUP, "LAN MATCH");
+                            MenuManager.SetDefaultSelection(0);
+                            MenuManager.m_menu_sub_state = MenuSubState.ACTIVE;
+                            */
+
+                            // --
+
+                            // -- Create match outright (LAN)
+
+
+                            MenuManager.m_mp_status = Loc.LS("CREATING LAN MATCH");
+                            PrivateMatchDataMessage pmd = GetPrivateMatchDataMessage();
+                            NetworkMatch.StartPrivateLobby(pmd);
+                            UIManager.DestroyAll(false);
+
+
+
+                            /*
+                             // -----------------
+                          
+
+                            GameManager.m_game_state = GameManager.GameState.MENU;
+                            //GameplayManager.GamePaused = true;
+
+
+                            //UIManager.m_menu_selection = 1;
+                            UIManager.DestroyAll(false);
+                            MenuManager.ChangeMenuState(MenuState.MP_LOCAL_MATCH, false);
+                            MenuManager.m_menu_micro_state = 4; 
+                            // create match
+                                                                
+                            //MenuManager.m_menu_sub_state = MenuSubState.INIT;
+
+
+                            // TODO - menu is not loading correctly - are menu elements not populated?
+
+                            //MenuManager.m_menu_micro_state = 0; // create match first step
+
+                            NetworkMatch.SetNetworkGameClientMode(NetworkMatch.NetworkGameClientMode.LocalLAN);
+
+                            GameplayManager.IsMultiplayerActive = false;
+                            MenuManager.m_mp_lan_match = true;
+                            MenuManager.m_mp_private_match = true;
+
+                            //MPInternet.Enabled
+                            //MPInternet.MenuPassword = "127.0.0.1";
+                            MenuManager.m_updating_pm_settings = false;
+
+
+                            MenuManager.PlaySelectSound(1f);
+                            UIManager.m_mouse_menu_selection = -1;
+
+
+                            // -----------------
+                            */
+
+
+                            //UIManager.Update();
+
+                            // ---
+
+                            //var x = MenuState.MP_LOCAL_MATCH;
+                            //UIManager.m_menu_selection = 7;
+                            //MenuManager.m_mp_lan_match = true;
+                            //(MenuManager.m_menu_micro_state == 0 && UIManager.m_menu_selection == 2) // create open match
+
+
+
+                        }
+
+                        //Debug.Log("KeyCode down: " + keyCode);
+                        break;
                         }
                     }
                 }
@@ -102,7 +170,60 @@ namespace GameMod.MPBotFramework
 
                 return !BotControl.Enabled;
             }
-        }
+
+            static PrivateMatchDataMessage GetPrivateMatchDataMessage() 
+            {
+                PrivateMatchDataMessage privateMatchDataMessage = new PrivateMatchDataMessage();
+                privateMatchDataMessage.m_name = "spark"; // PilotManager.PilotName;
+                privateMatchDataMessage.m_password = "127.0.0.1"; //MenuManager.mms_match_password;
+                privateMatchDataMessage.m_match_mode = MatchMode.ANARCHY; //MenuManager.mms_mode;
+                privateMatchDataMessage.m_num_players_to_start = 2;
+                privateMatchDataMessage.m_max_players_for_match = 2; //MenuManager.mms_max_players;
+                privateMatchDataMessage.m_addon_level_name_hash = "QUICK1.MP:9667D24A"; //string.Empty;
+
+                privateMatchDataMessage.m_level_num = -1;
+
+                            /*
+                if (GameManager.MultiplayerMission.IsLevelAnAddon(MenuManager.mms_level_num))
+                {
+                if (isOnlineMatch)
+                {
+                    privateMatchDataMessage.m_level_num = 0;
+                }
+                else
+                {
+                    privateMatchDataMessage.m_level_num = -1;
+                    privateMatchDataMessage.m_addon_level_name_hash = GameManager.MultiplayerMission.GetAddOnLevelIdStringHash(MenuManager.mms_level_num);
+                }
+                }
+                else
+                {
+                privateMatchDataMessage.m_level_num = MenuManager.mms_level_num;
+                }
+                */
+                privateMatchDataMessage.m_time_limit = MatchTimeLimit.NONE;//MenuManager.mms_time_limit;
+                privateMatchDataMessage.m_score_limit = 0; //MenuManager.mms_score_limit;
+                privateMatchDataMessage.m_respawn_time_seconds = 2; //MenuManager.mms_respawn_time;
+                privateMatchDataMessage.m_respawn_shield_time_seconds = 2; //MenuManager.mms_respawn_invuln;
+                privateMatchDataMessage.m_friendlyfire = false; //(MenuManager.mms_friendly_fire > 0);
+                privateMatchDataMessage.m_show_enemy_names = MatchShowEnemyNames.NORMAL; //MenuManager.mms_show_names;
+                privateMatchDataMessage.m_turn_speed_limit = 2; //MenuManager.mms_turn_speed_limit;
+                privateMatchDataMessage.m_powerup_spawn = 2; // MenuManager.mms_powerup_spawn;
+                privateMatchDataMessage.m_powerup_initial = 2; // MenuManager.mms_powerup_initial;
+                privateMatchDataMessage.m_powerup_big_spawn = 1; //MenuManager.mms_powerup_big_spawn;
+                privateMatchDataMessage.m_powerup_filter_bitmask = 0xffff; //RUtility.BoolArrayToBitmask(MenuManager.mms_powerup_filter);
+                privateMatchDataMessage.m_force_loadout = 0;  // MenuManager.mms_force_loadout;
+                privateMatchDataMessage.m_force_m1 = MissileType.FALCON;  //MenuManager.mms_force_m1;
+                privateMatchDataMessage.m_force_m2 = MissileType.NUM; // MenuManager.mms_force_m2;
+                privateMatchDataMessage.m_force_modifier1 = 4; //MenuManager.mms_force_modifier1;
+                privateMatchDataMessage.m_force_modifier2 = 4; //MenuManager.mms_force_modifier2;
+                privateMatchDataMessage.m_force_w1 = WeaponType.IMPULSE; //MenuManager.mms_force_w1;
+                privateMatchDataMessage.m_force_w2 = WeaponType.NUM; //MenuManager.mms_force_w2;
+
+                return privateMatchDataMessage;
+            }
+
+    }
 
         [HarmonyPatch(typeof(Controls), "MouseAim")]
         class PIgnoreControlsForGame3
@@ -152,9 +273,13 @@ namespace GameMod.MPBotFramework
             public static string msg = "Key press: NONE";
             public static string msg1 = "BOT CONTROL: OFF";
 
+            public static string msg2 = "STATE: NONE";
+
+        private static Vector2 scrollViewVector = Vector2.zero;
+            public  static string LogText = "";
 
 
-            public static void DrawRect(Rect position, Color color, GUIContent content = null)
+        public static void DrawRect(Rect position, Color color, GUIContent content = null)
             {
                 var backgroundColor = GUI.backgroundColor;
                 GUI.backgroundColor = color;
@@ -185,8 +310,29 @@ namespace GameMod.MPBotFramework
                 GUI.Label(new Rect(25, 25, 300, 30), msg, labelStyle);
 
                 GUI.Label(new Rect(25, 55, 300, 30), msg1, labelStyle);
-                
-            }
+
+                msg2 = "";
+                msg2 += $"UIManager.m_menu_selection: {UIManager.m_menu_selection}";
+                msg2 += $"\nUIManager.m_mouse_menu_selection: {UIManager.m_mouse_menu_selection}";
+                msg2 += $"\nMenuManager.m_menu_state: {MenuManager.m_menu_state}";
+                msg2 += $"\nMenuManager.m_menu_sub_state: {MenuManager.m_menu_sub_state}";
+                msg2 += $"\nMenuManager.m_menu_micro_state: {MenuManager.m_menu_micro_state}";
+                msg2 += $"\nMenuManager.m_menu_next_menu_state: {MenuManager.m_next_menu_state}";
+                msg2 += $"\nMenuManager.m_mp_lan_match: {MenuManager.m_mp_lan_match}";
+
+
+                GUI.Label(new Rect(25, 85, 900, 350), msg2, labelStyle);
+
+                scrollViewVector = GUI.BeginScrollView(new Rect(25, 485, 900, 400), scrollViewVector, new Rect(0, 0, 900, 900));
+
+                // Put something inside the ScrollView
+                LogText = GUI.TextArea(new Rect(0, 0, 900, 900), LogText);
+
+                // End the ScrollView
+                GUI.EndScrollView();
+
+
+    }
         }
 
         /*
